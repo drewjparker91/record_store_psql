061443a51106da1396cc859aa111853263fbc5ba
drewparker <drewjparker91@gmail.com>
2020-10-06 11:42:24 -0700

    Add functions to artist.rb for update and save Co-Authored-By: Adam Mansell <adammansellhmb@gmail.com> Co-Authored-By: Ophelia Swen <ophelia.swen@gmail.com>

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,10 +2,14 @@
 class Artist
   attr_reader :id
   attr_accessor :name
+  # :artist_id
+  #  :album_id
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
+    # @artist_id = attributes.fetch(:artist_id)
+    # @album_id = attributes.fetch(:album_id)
   end
 
   def ==(artist_to_compare)
@@ -25,16 +29,28 @@ class Artist
       artists.push(Artist.new({:name => name, :id => id}))
     end
     artists
-
   end
 
   def self.clear()
     DB.exec("DELETE FROM artists *;")
   end
 
-  def self.find()
+  def update(attributes)
+    if (attributes.has_key?(:name)) && (attributes.fetch(:name) != nil)
+      @name = attributes.fetch(:name)
+      DB.exec("UPDATE artists SET name = '#{@name}' WHERE id = #{@id};")
+    elsif (attributes.has_key?(:album_name)) && (attributes.fetch(:album_name) != nil)
+      album_name = attributes.fetch(:album_name)
+      album = DB.exec("SELECT * FROM albums WHERE lower(name)='#{album_name.downcase}';").first
+      if album != nil
+        DB.exec("INSERT INTO albums_artists (album_id, artist_id) VALUES (#{album['id'].to_i}, #{@id});")
+      end
+    end
+  end
+
+  def self.find(id)
     artist = DB.exec("SELECT * FROM artists WHERE id = #{id};").first
-    if album
+    if artist
       name = artist.fetch("name")
       id = artist.fetch("id").to_i
       Artist.new({:name => name, :id => id})
@@ -52,6 +68,29 @@ class Artist
     DB.exec("DELETE FROM artists WHERE id = #{@id}")
     DB.exec("DELETE FROM albums WHERE id = #{@id};")
     DB.exec("DELETE FROM songs WHERE album_id = #{@id};")
+  end
+
+  def albums
+    albums = []
+    results = DB.exec("SELECT album_id FROM albums_artists WHERE artist_id = #{@id};")
+    results.each() do |result|
+      album_id = result.fetch("album_id").to_i()
+      album = DB.exec("SELECT * FROM albums WHERE id = #{album_id};")
+      name = album.first().fetch("name")
+      albums.push(Album.new({:name => name, :id => album_id}))
+    end
+    albums
+  end
+
+  def self.find_by_artist(art_id)
+    albums = []
+    results = DB.exec("SELECT album_id FROM albums_artists WHERE artist_id = #{id};")
+    results.each() do |result|
+      album_id = result.fetch("album_id").to_i()
+      album = DB.exec("SELECT * FROM albums WHERE id = #{album_id};")
+      albums.push(Album.new({:name => name, :id => album_id}))
+    end
+    albums
   end
 end
 
--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -12,9 +12,7 @@ describe '#Artist' do
     it("saves an artist") do
       artist = Artist.new({:name => "Outkast", :id => nil})
       artist.save()
-      artist2 = Artist.new({:name => "Big Chungus", :id => nil})
-      artist2.save()
-      expect(Artist.all).to(eq([artist, artist2]))
+      expect(Artist.all).to(eq([artist ]))
     end
   end
 
@@ -28,4 +26,76 @@ describe '#Artist' do
       expect(Artist.all).to(eq([]))
     end
   end
+
+  describe('#==') do
+    it("is the same artist if it has the same attributes as another artist") do
+      artist = Artist.new({:name => "Blue October", :id => nil})
+      artist2 = Artist.new({:name => "Blue October", :id => nil})
+      expect(artist).to(eq(artist2))
+    end
+  end
+
+  describe('.find') do
+    it("finds an artist by id") do
+      artist = Artist.new({:name => "Outkast", :id => nil})
+      artist.save()
+      artist2 = Artist.new({:name => "Blue October", :id => nil})
+      artist2.save()
+      expect(Artist.find(artist.id)).to(eq(artist))
+    end
+  end
+
+  describe('.find') do
+    it("returns nil if user searches for an artist id that doesn't exist") do
+    artist = Artist.new({:name => "Outkast", :id => nil})
+    artist.save()
+    artist.delete()
+    expect(Artist.find(artist.id)).to(eq(nil))
+    end
+  end
+
+  describe('#update') do
+    it("updates an artist by id") do
+      artist = Artist.new({:name => "Outkast", :id => nil})
+      artist.save()
+      artist.update(:name => "Big Boi")
+      expect(artist.name).to(eq("Big Boi"))
+    end
+  end
+
+  describe('#update') do
+  it("adds an album to an artist") do
+    artist = Artist.new({:name => "John Coltrane", :id => nil})
+    artist.save()
+    album = Album.new({:name => "A Love Supreme", :id => nil})
+    album.save()
+    artist.update({:album_name => "A Love Supreme"})
+    expect(artist.albums).to(eq([album]))
+  end
+end
+
+  describe('#songs') do
+    it("returns an artist's songs") do
+      artist = Artist.new({:name => "Outkast", :id => nil})
+      artist.save()
+      song = Song.new({:name => "Naima", :artist_id => artist.id, :id => nil})
+      song.save()
+      song2 = Song.new({:name => "Cousin Mary", :artist_id => artist.id, :id => nil})
+      song2.save()
+      expect(artist.songs).to(eq([song, song2]))
+    end
+  end
+
+  describe('#delete') do
+    it("deletes an artist by id") do
+      artist = Artist.new({:name => "Outkast", :id => nil})
+      artist.save()
+      artist2 = Artist.new({:name => "Blue October", :id => nil})
+      artist2.save()
+      artist.delete()
+      expect(Artist.all).to(eq([artist2]))
+    end
+  end
+
+
 end
